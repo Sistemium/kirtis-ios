9f7056fa15616a6d816b861114f5b0ef80a3f997
Edgar Jan <krodanix@gmail.com>
2015-11-17 14:40:20 +0200

    Threads and spinner

---
+++ kirtis-ios/kirtis-ios/SpinnerTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SpinnerTableViewCell.swift
+//  kirtis-ios
+//
+//  Created by Edgar Jan Vuicik on 17/11/15.
+//  Copyright © 2015 Edgar Jan Vuicik. All rights reserved.
+//
+
+import UIKit
+
+class SpinnerTableViewCell: UITableViewCell {
+
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+}
