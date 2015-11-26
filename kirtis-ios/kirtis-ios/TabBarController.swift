6a6d6ab0968831236af628bc8dd63ca31d8e4996
Edgar Jan <krodanix@gmail.com>
2015-11-26 16:49:02 +0200

    Fixed change Language icons bug

---
+++ kirtis-ios/kirtis-ios/TabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  TabBarController.swift
+//  kirtis-ios
+//
+//  Created by Edgar Jan Vuicik on 26/11/15.
+//  Copyright © 2015 Sistemium. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    private let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let currentLanguageBundle = NSBundle(path:NSBundle.mainBundle().pathForResource(self.appDelegate.userLanguage , ofType:"lproj")!)
+        self.viewControllers?[0].tabBarItem.image = UIImage(named: NSLocalizedString("Home", bundle: currentLanguageBundle!, value: "Home", comment: "Home"))
+        self.viewControllers?[1].tabBarItem.image = UIImage(named: NSLocalizedString("Settings", bundle: currentLanguageBundle!, value: "Settings", comment: "Settings"))
+        self.viewControllers?[2].tabBarItem.image = UIImage(named: NSLocalizedString("About", bundle: currentLanguageBundle!, value: "About", comment: "About"))
+    }
+
+}
